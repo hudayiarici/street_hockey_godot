1e920d36ee2b536ab02d79c31fda01faa0e1696e
Hüdayi ARICI <hudayiarici@Hudayi-MacBook-Pro.local>
2025-12-10 07:40:15 +0300

    Refactor input handling in start screen for improved visibility check

--- scripts/start_screen.gd
+++ scripts/start_screen.gd
@@ -7,8 +7,9 @@ func _ready():
 	show()
 
 func _process(delta):
-	if Input.is_action_just_pressed("ui_accept"):  # Space key
-		start_game.emit()
-		hide()
-	elif Input.is_action_just_pressed("KEY_E"):  # E key
-		quit_game.emit()
+	if visible:
+		if Input.is_action_just_pressed("ui_accept"):  # Space key
+			start_game.emit()
+			hide()
+		elif Input.is_action_just_pressed("KEY_E"):  # E key
+			quit_game.emit()
